f9683fbf9621572cafb4b202937e3952655a9684
José Hildebrando <jose.hildebrando@gmail.com>
2021-04-09 15:31:00 -0300

    catching multiple datas

--- RealTimeStock/stock.py
+++ RealTimeStock/stock.py
@@ -28,13 +28,39 @@ def real_time_price(stock_code):
         if texts != []:
             price, change = texts[0], texts[1] # [0] = price, [1] = rice changes
         else:
-            print('tá caindo no else')
             price, change = [], []
-    except ConnectionError: # In case of a connection error when trying to get the content
-        price, change = [], [] # In case of there is no content
-        print("tá caindo no except")
 
-    return price, change
+
+
+        texts = web_content_div(web_content, 'D(ib) W(1/2) Bxz(bb) Pend(12px) Va(t) ie-7_D(i) smartphone_D(b) smartphone_W(100%) smartphone_Pend(0px) smartphone_BdY smartphone_Bdc($seperatorColor)')
+        if texts != []:
+            for  count, vol in enumerate(texts):
+                if vol == 'Volume':
+                    volume = texts[count + 1]
+        else:
+            volume = []
+
+        pattern = web_content_div(web_content, 'Fz(xs) Mb(ppx)')
+        try: # Trying if it is sending information inside this pattern
+            latest_pattern = pattern[0]
+        except IndexError:
+            latest_pattern = []
+
+        texts = web_content_div(web_content, 'D(ib) W(1/2) Bxz(bb) Pstart(12px) Va(t) ie-7_D(i) ie-7_Pos(a) smartphone_D(b) smartphone_W(100%) smartphone_Pstart(0px) smartphone_BdB smartphone_Bdc($seperatorColor)')
+        if texts != []:
+            for count, target in enumerate(texts):
+                if target == '1y Target Est':
+                    one_year_target =  texts[count + 1]
+        else:
+            one_year_target = []
+
+
+
+
+    except ConnectionError: # In case of a connection error when trying to get the content
+        price, change, volume, latest_pattern, one_year_target = [], [], [], [], [] # In case of there is no content
+
+    return price, change, volume, latest_pattern, one_year_target
 
 
 print(real_time_price(Stock[0]))
