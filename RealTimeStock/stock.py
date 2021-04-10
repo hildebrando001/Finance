b8fe6918885734c706e7743d377d856d75552942
José Hildebrando <jose.hildebrando@gmail.com>
2021-04-10 19:34:59 -0300

    getting multiple data

--- RealTimeStock/stock.py
+++ RealTimeStock/stock.py
@@ -4,7 +4,7 @@ import requests
 from requests.exceptions import ConnectionError
 from bs4 import BeautifulSoup
 
-Stock = ['BRK-B', 'PETR4.SA']
+Stock = ['BRK-B', 'PETR4.SA', 'TWTR', 'AAPL', 'AMZN', 'FB', 'GOOG', 'MSFT', 'PYPL']
 
 class_path = 'My(6px) Pos(r) smartphone_Mt(6px)'
 
@@ -13,7 +13,7 @@ def web_content_div(web_content, class_path):
     web_content_div = web_content.find_all('div', {'class':class_path}) # search all the div with this class
     try:
         spans = web_content_div[0].find_all('span') # find all the span inside the div
-        texts = [span.get_text() for span in spans] # create a list with content of the spans
+        texts = [sp.get_text() for sp in spans] # create a list with content of the spans
     except IndexError:
         texts = []
     return texts
@@ -40,7 +40,7 @@ def real_time_price(stock_code):
         else:
             volume = []
 
-        pattern = web_content_div(web_content, 'Fz(xs) Mb(ppx)')
+        pattern = web_content_div(web_content, 'Fz(xs) Mb(ppx)') 
         try: # Trying if it is sending information inside this pattern
             latest_pattern = pattern[0]
         except IndexError:
@@ -56,12 +56,31 @@ def real_time_price(stock_code):
 
 
 
-
     except ConnectionError: # In case of a connection error when trying to get the content
         price, change, volume, latest_pattern, one_year_target = [], [], [], [], [] # In case of there is no content
 
     return price, change, volume, latest_pattern, one_year_target
 
 
-print(real_time_price(Stock[0]))
-print(real_time_price(Stock[1]))
+# print(real_time_price(Stock[0]))
+# print(real_time_price(Stock[1]))
+
+
+while (True):
+    info = []
+    col  = []
+    time_stamp = datetime.datetime.now() # - datetime.timedelta(hours=11) # difference between Hong kong and Brazil. When tested out with HK market.
+    time_stamp = time_stamp.strftime('%Y-%m-%d %H:%M:%S')
+    for stock_code in Stock:
+        stock_price, change, volume, latest_pattern, one_year_target = real_time_price(stock_code)
+        info.append(stock_price)
+        info.extend([change])
+        info.extend([volume])
+        info.extend([latest_pattern])
+        info.extend([one_year_target])
+    col = [time_stamp]
+    col.extend(info)
+    df = pd.DataFrame(col)
+    df = df.T
+    df.to_csv(str(time_stamp[0:11]) + 'stockdata.csv', mode='a', header=False)
+    print(col)
